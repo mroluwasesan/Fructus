7516050cc31887481264cf6ea58d43f0b0a3486c
Rotimi <mr.oluwasesan@gmail.com>
2023-04-17 23:04:33 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Negatron on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
