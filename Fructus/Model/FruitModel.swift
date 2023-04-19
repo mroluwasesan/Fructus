6af30f8523efb90c9ef8054c5115a2938de43dc2
Rotimi <mr.oluwasesan@gmail.com>
2023-04-19 11:17:32 +0100

    creating a data model and fetching of data using swift

---
+++ Fructus/Model/FruitModel.swift
@@ -0,0 +1,24 @@
+//
+//  FruitModel.swift
+//  Fructus
+//
+//  Created by Negatron on 19/04/2023.
+//
+
+import SwiftUI
+
+// MARK: - FRUITS DATA MODEL
+
+struct Fruit: Identifiable{
+    
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+    
+    
+}
+
--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -9,28 +9,31 @@ import SwiftUI
 
 struct FruitCardView: View {
     //MARK: - PTOPERTIES
+    
+    var fruit: Fruit
     @State private var isAnimating: Bool = false
+    
     //MARK: - BODY
     
     var body: some View {
         ZStack {
             VStack(spacing: 20) {
                 // FRUIT: IMAGE
-                Image("blueberry")
+                Image(fruit.image)
                     .resizable()
                     .scaledToFit()
                     .shadow(color:  Color(red: 0,green: 0, blue: 0,opacity: 0.15),radius: 8, x: 6, y: 8)
                 
                 
                 // FRUIT: TITLE
-                Text("Blueberry")
+                Text(fruit.title)
                     .foregroundColor(Color.white)
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
                     .scaleEffect(isAnimating ? 1.0 : 0.6)
                 // FRUIT: HEADLINE
-                Text("Blueberry are sweet, nutritious and wildly popular fruit all over the world.")
+                Text(fruit.headline)
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 70)
@@ -46,7 +49,7 @@ struct FruitCardView: View {
             }
         }
         .frame(minWidth: 0,maxWidth: .infinity, minHeight: 0,maxHeight: .infinity,alignment: .center)
-        .background(.linearGradient(Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .background(.linearGradient(Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(20)
         .padding(.horizontal,20)
       
@@ -59,7 +62,7 @@ struct FruitCardView: View {
 
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitCardView()
+        FruitCardView(fruit: fruitsData[4])
             .previewLayout(.fixed(width: 320, height: 640))
     }
 }
